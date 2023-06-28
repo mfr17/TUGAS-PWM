1ebbba5d06e4279e770cc0edcc3d5f1cf5599121
Muhammad Dzikri <47478173+mfr17@users.noreply.github.com>
2023-06-28 14:02:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# TUGAS-PWM
+
+Untuk sistem validasi saya membuat sendiri dengan js agar dapat login ke dashboard (walaupun masih kurang middleware), lalu untuk dashboard mungkin belum berjalan karena saya belum sempat untuk mengkonfigurasi API saya.
---
+++ assets/build/host.js
@@ -0,0 +1,37 @@
+const endpoint = "http://localhost:3000";
+async function getHs_Host() {
+    const host = await fetch(endpoint + "/ip/hotspot/host");
+    const host_res = await host.json();
+    if (host_res.length >= 0) {
+        var temp = "";
+        i = 0;
+        host_res.forEach((itemData) => {
+            i++;
+            temp += "<tr>";
+            temp += "<td>" + i;
+            // temp += "<td>" + itemData[".id"];
+            if (itemData["authorized"] == "true") {
+                temp += "<td> AH";
+            } else if (itemData["static"] == "true" && itemData["bypassed"] == "true") {
+                temp += "<td> PS";
+            } else if (itemData["authorized"] == "false" && itemData["bypassed"] == "true") {
+                temp += "<td> P";
+            } else {
+                temp += "<td> H";
+            }
+            temp += "<td>" + itemData["server"];
+            temp += "<td>" + itemData["mac-address"];
+            temp += "<td>" + itemData["address"];
+            temp += "<td>" + itemData["to-address"];
+            if (itemData["comment"]) {
+                temp += "<td>" + itemData["comment"];
+            } else {
+                temp += "<td> No Comment";
+            }
+            temp += "</td></tr>";
+        });
+        document.getElementById("total_host").innerHTML = host_res.length;
+        document.getElementById("tb_host").innerHTML = temp;
+    }
+}
+getHs_Host();
